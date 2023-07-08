63be50b3cc8343399efe356291d25a2764c217cf
Mayte Ozoria <mayteozoria@gmail.com>
2023-07-08 00:55:58 -0400

    Backend added User

---
+++ middleware/index.js
@@ -0,0 +1,60 @@
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
+
+const SALT_ROUNDS = parseInt(process.env.SALT_ROUNDS)
+const APP_SECRET = process.env.APP_SECRET
+
+const hashPassword = async (password) => {
+  let hashedPassword = await bcrypt.hash(password, SALT_ROUNDS)
+  return hashedPassword
+}
+
+const comparePassword = async (storedPassowrd, password) => {
+  let passwordMatch = await bcrypt.compare(password, storedPassowrd)
+  return passwordMatch
+}
+
+const createToken = (payload) => {
+  let token = jwt.sign(payload, APP_SECRET)
+  return token
+}
+
+const verifyToken = (req, res, next) => {
+  const { token } = res.locals
+  try {
+    let payload = jwt.verify(token, APP_SECRET)
+    if (payload) {
+      res.locals.payload = payload
+      return next()
+    }
+    res.status(401).send({ status: 'Error', msg: 'Unauthorized' })
+  } catch (error) {
+    console.log(error)
+    res.status(401).send({ status: 'Error', msg: 'Verify Toek Error!' })
+  }
+}
+
+const stripToken = (req, res, next) => {
+  try {
+    const token = req.headers['authorization'].split(' ')[1]
+    // Gets the token from the request headers {authorization: Bearer Some-Token}
+    // Splits the value of the authorization header
+    if (token) {
+      res.locals.token = token
+      // If the token exists we add it to the request lifecycle state
+      return next()
+    }
+    res.status(401).send({ status: 'Error', msg: 'Unauthorized' })
+  } catch (error) {
+    console.log(error)
+    res.status(401).send({ status: 'Error', msg: 'Strip Token Error!' })
+  }
+}
+module.exports = {
+  hashPassword,
+  comparePassword,
+  createToken,
+  stripToken,
+  verifyToken
+}
