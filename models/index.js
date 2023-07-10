6504e011168db289c5373e6253f475fa561f212f
Mayte Ozoria <mayteozoria@gmail.com>
2023-07-09 23:43:49 -0400

    Step Controller

--- models/index.js
+++ models/index.js
@@ -2,13 +2,16 @@ const mongoose = require('mongoose')
 const userSchema = require('./User')
 const postSchema = require('./Post')
 const foodSchema = require('./Food')
+const stepSchema = require('./Step')
 
 const User = mongoose.model('User', userSchema)
 const Post = mongoose.model('Post', postSchema)
 const Food = mongoose.model('Food', foodSchema)
+const Step = mongoose.model('Step', stepSchema)
 
 module.exports = {
   User,
   Post,
-  Food
+  Food,
+  Step
 }
