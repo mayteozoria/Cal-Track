301628e3f7ff72173aa439629e72b63a051272cd
Mayte Ozoria <mayteozoria@gmail.com>
2023-07-11 12:02:54 -0400

    Updated Food Model

--- models/Food.js
+++ models/Food.js
@@ -3,14 +3,14 @@ const { Schema } = require('mongoose')
 const foodSchema = new Schema(
   {
     name: String,
-    numberOfServings: Number,
-    servingSize: Number,
     calories: Number,
-    carbs: Number,
-    fats: Number,
-    protein: Number,
-    sodium: Number,
-    sugar: Number
+    service_size_g: Number,
+    protein_g: Number,
+    carbohydrates_total_g: Number,
+    fat_total_g: Number,
+    fiber_g: Number,
+    sodium_mg: Number,
+    sugar_g: Number
   },
   { timestamps: true }
 )
