01deb8548e31ce36f3ae676f26d5799b1a61975f
Mayte Ozoria <mayteozoria@gmail.com>
2023-07-20 10:30:27 -0400

    new prep

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,5 +1,5 @@
 import Axios from 'axios'
-export const BASE_URL = 'http://localhost:3001'
+export const BASE_URL = 'https://caltrack-4cf339ded871.herokuapp.com/'
 
 const Client = Axios.create({ baseURL: BASE_URL })
 
--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -3,25 +3,23 @@ const middleware = require('../middleware')
 
 const Register = async (req, res) => {
   try {
-    // Extracts the necessary fields from the request body
     const { email, password, firstName, lastName } = req.body
-    // Hashes the provided password
+
     let passwordDigest = await middleware.hashPassword(password)
-    // Checks if there has already been a user registered with that email
+
     let existingUser = await User.findOne({ email })
     if (existingUser) {
       return res
         .status(400)
         .send('A user with that email has already been registered!')
     } else {
-      // Creates a new user
       const user = await User.create({
         firstName,
         lastName,
         email,
         passwordDigest
       })
-      // Sends the user as a response
+
       res.send(user)
     }
   } catch (error) {
